c362dd9c6f28e7813a60c0706354ffc980ef344a
demoprojset2 <97522797+demoprojset2@users.noreply.github.com>
2022-01-24 10:48:20 +0530

    Unit-test Updated

--- test/Drlogintest.py
+++ test/Drlogintest.py
@@ -1,29 +1,29 @@
 from selenium import webdriver
-import HtmlTestRunner
+import HtmlTestRunner 
 import time
 import unittest
 
 class login(unittest.TestCase):
 
     def test_drvalidlogin(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("http://127.0.0.1:3000/sign-in") 
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://xenodochial-gates-c9614f.netlify.app/sign-in") 
         time.sleep(1)
         userid=self.driver.find_element_by_xpath('//*[@id="name"]')
-        userid.send_keys("gawande@gmail.com")
+        userid.send_keys("demo2@gmail.com")
         time.sleep(1)
         password=self.driver.find_element_by_xpath('//*[@id="password"]')
-        password.send_keys("Aishwarya")
+        password.send_keys("sparshgoyal")
         time.sleep(1)
         signin=self.driver.find_element_by_xpath('//*[@id="root"]/div/div/div[3]/div/form/div[3]/button')
         signin.click()
         time.sleep(2)
   
-        self.assertEqual(self.driver.current_url, "http://127.0.0.1:3000/docdash")
+        self.assertEqual(self.driver.current_url, "https://xenodochial-gates-c9614f.netlify.app/docdash")
    
     def test_drinvalidlogin(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("http://127.0.0.1:3000/sign-in") 
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://xenodochial-gates-c9614f.netlify.app/sign-in") 
         time.sleep(1)
         userid=self.driver.find_element_by_xpath('//*[@id="name"]')
         userid.send_keys("aishwarya@gmail.com")
@@ -35,7 +35,7 @@ class login(unittest.TestCase):
         signin.click()
         time.sleep(2)
   
-        self.assertEqual(self.driver.current_url, "http://127.0.0.1:3000/docdash")
+        self.assertEqual(self.driver.current_url, "https://xenodochial-gates-c9614f.netlify.app/docdash")
 
           
        
